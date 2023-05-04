b25c0b8b815f03b959e21c713d8cd3d2d63d328c
John <jjpyae@gmail.com>
2023-05-03 23:49:47 -0700

    Completed Phase I

--- guessing-game.js
+++ guessing-game.js
@@ -1,3 +1,23 @@
+//Create interfac for input and output using Node's readline module
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askGuess = function() {
+rl.question('Enter a guess: ', (answer) => {
+  let correct = checkGuess(Number(answer));
+  if (correct) {
+    console.log('You win!');
+    rl.close();
+  } else {
+    askGuess();
+  }
+});
+};
+askGuess();
+
 let secretNumber = 5;
 function checkGuess(number) {
   if (number > secretNumber) {
@@ -10,5 +30,8 @@ function checkGuess(number) {
     console.log('Correct!');
     return true;
   }
-
 }
+
+// console.log(checkGuess(10)) //too high; false;
+// console.log(checkGuess(0)) //too low; false;
+// console.log(checkGuess(5)) //correct; true;
